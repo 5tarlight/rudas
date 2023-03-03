4dc38477252b904a902f544242cf583a59d8c59f
YEAHx4 <developerstarlight@gmail.com>
2023-03-04 05:18:21 +0900

    clone with new label

--- src/data.rs
+++ src/data.rs
@@ -1,10 +1,11 @@
+use core::panic;
 use std::{any::type_name, fmt::Display};
 
 #[derive(Debug, Clone)]
 /// One-dimensional array with axis labels.
 pub struct Series<T: Clone, U: PartialEq> {
-    data: Vec<T>,
-    labels: Vec<U>,
+    pub data: Vec<T>,
+    pub label: Vec<U>,
 }
 
 impl<T: Clone, U: PartialEq> Series<T, U> {
@@ -13,7 +14,7 @@ impl<T: Clone, U: PartialEq> Series<T, U> {
     pub fn from(v: &[T]) -> Series<T, usize> {
         Series {
             data: v.to_vec(),
-            labels: (0..v.len()).collect(),
+            label: (0..v.len()).collect(),
         }
     }
 
@@ -31,7 +32,50 @@ impl<T: Clone, U: PartialEq> Series<T, U> {
 
         Series {
             data: v.to_vec(),
-            labels: l.to_vec(),
+            label: l.to_vec(),
+        }
+    }
+
+    /// Create new [`Series`] with same data and label of given Series.
+    /// Works same as `.clone()`
+    pub fn from_self(s: &Series<T, U>) -> Series<T, U>
+    where
+        U: Clone,
+    {
+        Series {
+            data: s.data.clone(),
+            label: s.label.clone(),
+        }
+    }
+
+    /// Create new [`Series`] instance with data and default label.
+    ///
+    /// Works same as
+    /// ```
+    /// use rudas::data::Series;
+    /// let s = Series::from_label(&[1, 2, 3], &[10, 20, 30]);
+    /// let d = Series::from_data(&s);
+    /// assert_eq!(d.label[1], 1);
+    /// ```
+    pub fn from_data(s: &Series<T, U>) -> Series<T, usize> {
+        Series {
+            data: s.data.clone(),
+            label: (0..s.data.len()).collect(),
+        }
+    }
+
+    /// Create new [`Series`] instance with data and new label.
+    ///
+    /// # Panics
+    /// Length of original `Series` instance and new label should be equal.
+    pub fn from_data_new_label<L: PartialEq + Clone>(s: &Series<T, U>, l: &[L]) -> Series<T, L> {
+        if s.data.len() != l.len() {
+            panic!("Length of data and label should be equal.");
+        }
+
+        Series {
+            data: s.data.clone(),
+            label: l.to_vec(),
         }
     }
 
@@ -49,7 +93,7 @@ impl<T: Clone, U: PartialEq> Series<T, U> {
         U: Display,
     {
         for i in 0..self.data.len() {
-            println!("{:}\t{:}", self.labels[i], self.data[i]);
+            println!("{:}\t{:}", self.label[i], self.data[i]);
         }
 
         println!("type : {}", type_name::<T>());
@@ -79,7 +123,7 @@ mod test {
             }
         }
 
-        for (va, vb) in zip(a.labels.clone(), b.labels.clone()) {
+        for (va, vb) in zip(a.label.clone(), b.label.clone()) {
             if va != vb {
                 equal = false;
             }
@@ -100,5 +144,20 @@ mod test {
     #[should_panic]
     fn diffrent_length_of_data_and_label_should_panic() {
         Series::from_label(&[1, 2, 3], &[1]);
+    }
+
+    #[test]
+    fn duplicate_self_should_equal() {
+        let a = Series::from_label(&[1, 2, 3], &[1, 2, 3]);
+        let b = Series::from_self(&a);
+
+        assert!(equal(&a, &b));
+    }
+
+    #[test]
+    #[should_panic]
+    fn duplicate_with_differnet_length_should_panic() {
+        let a = Series::<i32, usize>::from(&[1, 2, 3]);
+        let _b = Series::from_data_new_label(&a, &[1]);
     }
 }
